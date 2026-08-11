d9b49e73b56aff543f126a704960899506df96a4
raywily110397@gmail.com <raywily110397@gmail.com>
2026-08-10 22:54:12 -0500

    adding fntion prendas

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ def home():
         "mensaje": "API funcionando",
         "version": "1.0"
     }
+@app.route("/prendas")
+def prendas():
+    return [
+        {
+            "id": 1,
+            "nombre": "Polo Negro",
+            "precio": 35
+        },
+        {
+            "id": 2,
+            "nombre": "Casaca Azul",
+            "precio": 80
+        }
+    ]
+
 
 if __name__ == "__main__":
     app.run(debug=True)
