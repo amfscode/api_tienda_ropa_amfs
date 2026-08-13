75d0afebb2aaf7254ab4032414ab845fd18c7d05
raywily110397@gmail.com <raywily110397@gmail.com>
2026-08-12 22:59:04 -0500

    update funtion prendas

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
+prendas = [
+{
+    "id": 1,
+    "nombre": "Polo negro",
+    "precio": 35
+},
+{
+    "id": 2,
+    "nombre": "Casaca",
+    "precio": 80
+},
+{
+    "id": 3,
+    "nombre": "Jean",
+    "precio": 120
+}
+]
 @app.route("/")# == http://127.0.0.1:5000/
 def home():
     # return "API funcionando"
@@ -9,19 +26,16 @@ def home():
     }
 @app.route("/prendas")
 def prendas():
-    return [
-        {
-            "id": 1,
-            "nombre": "Polo Negro",
-            "precio": 35
-        },
-        {
-            "id": 2,
-            "nombre": "Casaca Azul",
-            "precio": 80
-        }
-    ]
+    return prendas
 
+@app.route("/prendas/<int:id>")
+def obtener_prendas(id):
+    for prenda in prendas:
+        if prenda["id"] == id:
+            return prenda
+    return {
+        "error": "Prenda no encontrada"
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
